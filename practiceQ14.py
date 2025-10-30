efa18bb5f32f95d56ca30a9156f831dd56d06f77
girishshirsat <girishshirsath2004@gmail.com>
2025-10-31 00:04:43 +0530

    add door mat problem

---
+++ practiceQ14.py
@@ -0,0 +1,28 @@
+'''Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.'''
+
+a,b=input().split()
+N=int(a)
+M=int(b)
+if N%2==0 and M!=N*3:
+    print("Invalid Input ")
+    quit()
+else:
+
+    N, M = map(int, input().split())
+
+
+    for i in range(N // 2):
+        pattern = ".|." * (2 * i + 1)
+        print(pattern.center(M, '-'))
+
+
+    print("WELCOME".center(M, '-'))
+
+
+    for i in range(N // 2 - 1, -1, -1):
+        pattern = ".|." * (2 * i + 1)
+        print(pattern.center(M, '-'))

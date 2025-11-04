02b567383f2d4c400c1d3cc62184355db19bc112
Girish Shirsath <112466858+girishshirsat@users.noreply.github.com>
2025-11-05 00:56:20 +0530

    Update practiceQ17.py

--- practiceQ17.py
+++ practiceQ17.py
@@ -1,3 +1,14 @@
+"""Sample Input
+
+STDIN                                       Function
+-----                                       --------
+10                                          arr[] size N = 10
+161 182 161 154 176 170 167 171 170 174     arr = [161, 181, ..., 174]
+Sample Output
+
+169.375"""
+
+
 def average(array):
     s=set(array)
     av=sum(s)/len(s)

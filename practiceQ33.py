519dbf371c5e950f106d7408b22a8b3f2385e043
girishshirsat <girishshirsath2004@gmail.com>
2026-04-06 23:47:27 +0530

    polynomial evaluation

---
+++ practiceQ33.py
@@ -0,0 +1,34 @@
+"""Task
+
+You are given a polynomial  of a single indeterminate (or variable), .
+You are also given the values of  and . Your task is to verify if .
+
+Constraints
+All coefficients of polynomial  are integers.
+ and  are also integers.
+
+Input Format
+
+The first line contains the space separated values of  and .
+The second line contains the polynomial .
+
+Output Format
+
+Print True if . Otherwise, print False.
+
+Sample Input
+
+1 4
+x**3 + x**2 + x + 1
+Sample Output
+
+True"""
+
+x, k = map(int, input().split())
+poly = input()
+
+
+result = eval(poly)
+
+
+print(result == k)

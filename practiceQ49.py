70f70e06f0701725c3243db3c11d8e479d8ffbac
girishshirsat <girishshirsath2004@gmail.com>
2026-04-25 18:24:55 +0530

    merge the tools

---
+++ practiceQ49.py
@@ -0,0 +1,31 @@
+"""Consider the following:
+
+A string, , of length  where .
+An integer, , where  is a factor of .
+We can split  into  substrings where each subtring, , consists of a contiguous block of  characters in . Then, use each  to create string  such that:
+
+The characters in  are a subsequence of the characters in .
+Any repeat occurrence of a character is removed from the string such that each character in  occurs exactly once. In other words, if the character at some index  in  occurs at a previous index  in , then do not include the character in string .
+Given  and , print  lines where each line  denotes string """
+
+def merge_the_tools(string, k):
+   S=string
+   N=0
+   for i in range(0,len(S),+k):
+      N=N+k
+      A=S[i:N]
+      st=""
+      for j in A:
+         if j not in st:
+            st+=j
+      print(st)
+      st=""
+      
+     
+
+
+
+
+
+string, k = input(), int(input())
+merge_the_tools(string, k)
